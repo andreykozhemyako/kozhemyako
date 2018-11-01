38fa7db0029eb4c08d6714ac61cd0b3f5ce0cb57
andreykozhemyako <andrey_radrigo@mail.ru>
2018-11-01 16:17:52 +0400

    Добавил логирование в файл log в папке C://TMP

--- src/main/java/com/simbirsoft/itplace/Main.java
+++ src/main/java/com/simbirsoft/itplace/Main.java
@@ -3,10 +3,14 @@ package com.simbirsoft.itplace;
 import com.simbirsoft.itplace.service.api.SummaryService;
 import com.simbirsoft.itplace.service.impl.SummaryServiceImpl;
 
-public class Main {
 
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static final Logger log = Logger.getLogger(SummaryServiceImpl.class);
     public static void main(String[] args) {
         SummaryService summaryService = new SummaryServiceImpl("person.properties");
         summaryService.createHtmlFile("src/main/webapp/summary.html");
+        log.info("Программа скомпелирована!");
     }
 }
